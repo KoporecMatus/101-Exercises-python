ce495631952540443daae68fbb66b46765d3d9ff
Matúš Koporec <94967383+KoporecMatus@users.noreply.github.com>
2021-12-02 21:27:38 +0000

    Add files via upload

---
+++ 36.py
@@ -0,0 +1,7 @@
+def Countwords(filepath):
+    with open(filepath, "r") as file:
+        StrgFile=file.read()
+        strgList=StrgFile.split(" ")
+        return len(strgList)
+
+print(Countwords("C:/Users/kopor/OneDrive/Počítač/Python/101 Exercises/words1.txt"))
